4f368841806f01ffb7768f379c5ecd3ffd0f852d
Daniel Willett <drw@netcraft.com>
2026-01-27 16:06:32 +0000

    An introduction. Going through Q1,2,3,4,5,6,7,9,10,11,12,13,14 of Project Euler solutions https://github.com/daniel-willett/project-euler/tree/main/go

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # common-go-methods
-A library of common Golang functions which either are often used or are likely to be useful
+A library of common Golang functions which either are often used or are likely to be useful. A good source of inspiration is from projects already completed like Project Euler and also with the success Python has had to look at their docs for funcions and methods built in that would likely be useful. e.g.
+
+- https://docs.python.org/3/library/functions.html
+- https://www.w3schools.com/python/python_ref_functions.asp
---
+++ common.go
@@ -0,0 +1,55 @@
+package main
+
+import("math"; "strconv")
+
+//DEPENDS ON: Reverse
+func IsPalendrome(n int) bool{
+	var nstr string = strconv.Itoa(n)
+	if Reverse(nstr)==nstr{
+		return true
+	}
+	return false
+}
+
+func IsPrime(n int) bool{
+	var upper int = int(math.Sqrt(float64(n)))+1
+	for factor:=2; factor<upper; factor++{
+		if n%factor==0{
+			return false
+		}
+	}
+	return true
+}
+
+func Max(counter []int) int{
+	var largest int = 0
+	for _, val := range counter{
+		if val>largest{
+			largest=val
+		}
+	}
+	return largest
+}
+
+func NumOfDivs(x int) int{
+	var counter int = 0
+	var upper int = int(math.Sqrt(float64(n)))+1
+	for factor:=1; factor<upper; factor++{
+		if x%factor==0{
+			counter += 1
+		}
+	}
+	counter *= 2
+	if float64(x)==math.Sqrt(float(64)){
+		counter -= 1
+	}
+	return counter
+}
+
+func Reverse(nstr string) string{
+	var result string = ""
+	for counter := len(nstr)-1; counter>=0; counter--{
+		result += string(nstr[counter])
+	}
+	return result
+}
