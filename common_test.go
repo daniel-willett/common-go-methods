ce053ac935f86689ecdc1f409d8972a5f32f4278
Daniel Willett <drw@netcraft.com>
2026-02-03 10:35:08 +0000

    `All` & `Any` unit tests

--- common_test.go
+++ common_test.go
@@ -9,10 +9,10 @@ func TestAbs(t *testing.T){
 		expected	float64
 	}{
 		{"Integer", 5, 5},
-		{"Integer", -10, 10},
-		{"Integer", 0, 0},
-		{"Integer", 123.532,123.532},
-		{"Integer", -43.000001, 43.000001},
+		{"Negative Integer", -10, 10},
+		{"Zero", 0, 0},
+		{"Float", 123.532,123.532},
+		{"Negative Float", -43.000001, 43.000001},
 	}
 
 	for _, tt := range tests{
@@ -24,3 +24,48 @@ func TestAbs(t *testing.T){
 		})
 	}
 }
+
+func TestAll(t *testing.T){
+        tests := []struct{
+                name            string
+                input           []bool
+                expected        bool
+        }{
+                {"3 Trues", []bool{true,true,true}, true},
+                {"3 Falses", []bool{false,false,false}, false},
+                {"Empty", []bool{}, true}, //The empty intersection is the whole space so thus true here
+                {"One Of Each", []bool{true, false},false},
+        }
+
+        for _, tt := range tests{
+                t.Run(tt.name, func(t *testing.T){
+                        result := All(tt.input)
+                        if result != tt.expected{
+                                t.Errorf("All(%v) = %v; want %v", tt.input, result, tt.expected)
+                        }
+                })
+        }
+}
+
+func TestAny(t *testing.T){
+        tests := []struct{
+                name            string
+                input           []bool
+                expected        bool
+        }{
+                {"3 Trues", []bool{true,true,true}, true},
+                {"3 Falses", []bool{false,false,false}, false},
+                {"Empty", []bool{}, false}, //The empty union is the empty set so thus false here
+                {"One Of Each", []bool{true, false},true},
+        }
+
+        for _, tt := range tests{
+                t.Run(tt.name, func(t *testing.T){
+                        result := Any(tt.input)
+                        if result != tt.expected{
+                                t.Errorf("Any(%v) = %v; want %v", tt.input, result, tt.expected)
+                        }
+                })
+        }
+}
+
